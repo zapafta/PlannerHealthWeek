b3bdfec0b29febbffdb33b5494cb4696c6922ea0
Renato Santos <Renato Santos@DESKTOP-7EEPT47>
2023-05-27 11:54:21 +0100

    Planner health first commit

--- PlannerHealthWeek/Data/ApplicationDbContext.cs
+++ PlannerHealthWeek/Data/ApplicationDbContext.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PlannerHealthWeek.Data.Model;
 
 namespace PlannerHealthWeek.Data
 {
     public class ApplicationDbContext : IdentityDbContext
+
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+          : base(options)
         {
         }
+
+
+
+
+        public DbSet<ReceitaItem> ReceitaItem { get; set; } = default!;
+        public DbSet<MealPlan> MealPlan { get; set; } = default!;
+        public DbSet<MealPlanItem> MealPlanItem { get; set; } = default!;
+        public DbSet<Receita> Recipe { get; set; } = default!;
+        public DbSet<Ingrediente> Ingredient { get; set; } = default!;
+        public DbSet<TipoPlano> TipoPlano { get; set; } = default!;
+        public DbSet<TipoDieta> TipoDieta { get; set; } = default!;
+
     }
 }
